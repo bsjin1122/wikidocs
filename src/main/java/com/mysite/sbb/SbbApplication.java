e36ed9ea4bce3ca57319c60ab8f606d30bed7215
bsj <kitty7579@hanmail.net>
2024-07-08 23:49:35 +0900

    test

--- src/main/java/com/mysite/sbb/SbbApplication.java
+++ src/main/java/com/mysite/sbb/SbbApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SbbApplication {
 
 	public static void main(String[] args) {
+		System.out.println("test");
 		SpringApplication.run(SbbApplication.class, args);
 	}
 
