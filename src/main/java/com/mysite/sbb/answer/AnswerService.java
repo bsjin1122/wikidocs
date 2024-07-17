cac128d44494b765f310ca19e1596973dc7637ef
bsjin1122 <kitty7579@hanmail.net>
2024-07-17 12:00:48 +0900

    [3-09] 수정과 삭제 기능 추가하기

--- src/main/java/com/mysite/sbb/answer/AnswerService.java
+++ src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -2,9 +2,11 @@ package com.mysite.sbb.answer;
 
 import com.mysite.sbb.user.SiteUser;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.mysite.sbb.DataNotFoundException;
 import com.mysite.sbb.question.Question;
 
 import lombok.RequiredArgsConstructor;
@@ -12,9 +14,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Service
 public class AnswerService {
-	
+
 	private final AnswerRepository answerRepository;
-	
+
 	public Answer create(Question question, String content, SiteUser author) {
 		Answer answer = new Answer();
 		answer.setContent(content);
@@ -23,5 +25,24 @@ public class AnswerService {
 		answer.setAuthor(author);
 		this.answerRepository.save(answer);
 		return answer;
+	}
+
+	public Answer getAnswer(Integer id) {
+		Optional<Answer> answer = this.answerRepository.findById(id);
+		if (answer.isPresent()) {
+			return answer.get();
+		} else {
+			throw new DataNotFoundException("answer not found");
+		}
+	}
+
+	public void modify(Answer answer, String content) {
+		answer.setContent(content);
+		answer.setModifyDate(LocalDateTime.now());
+		this.answerRepository.save(answer);
+	}
+
+	public void delete(Answer answer) {
+		this.answerRepository.delete(answer);
 	}
 }
--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -19,9 +19,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Service
 public class QuestionService {
-	
+
 	private final QuestionRepository questionRepository;
-	
+
 	public List<Question> getList() {
 		return this.questionRepository.findAll();
 	}
@@ -35,13 +35,13 @@ public class QuestionService {
 
 	public Question getQuestion(Integer id) {
 		Optional<Question> question = this.questionRepository.findById(id);
-		if(question.isPresent()) {
+		if (question.isPresent()) {
 			return question.get();
 		} else {
 			throw new DataNotFoundException("question not found");
 		}
 	}
-	
+
 	public void create(String subject, String content, SiteUser user) {
 		Question q = new Question();
 		q.setSubject(subject);
@@ -49,5 +49,16 @@ public class QuestionService {
 		q.setCreateDate(LocalDateTime.now());
 		q.setAuthor(user);
 		this.questionRepository.save(q);
+	}
+
+	public void modify(Question question, String subject, String content) {
+		question.setSubject(subject);
+		question.setContent(content);
+		question.setModifyDate(LocalDateTime.now());
+		this.questionRepository.save(question);
+	}
+
+	public void delete(Question question) {
+		this.questionRepository.delete(question);
 	}
 }
